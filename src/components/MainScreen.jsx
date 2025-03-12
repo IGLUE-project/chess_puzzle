af4df1e347624daf2305de02b07bf5a7dad3548b
Javier Ferreño <javifc67@gmail.com>
2025-03-12 18:31:32 +0100

    limpieza de codigo

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,49 +1,16 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { ALLPICES } from "../constants/constants";
 import { cleanPiece, cleanSquare, saveSquare } from "../redux/ChessboardSlicer";
 import "./../assets/scss/MainScreen.scss";
 import Board from "./Board";
 import Box from "./Box";
-import { BOXPOSITION } from "../constants/constants";
 
 export default function MainScreen({ show }) {
   const dispatch = useDispatch();
   const [pieceDrag, setPieceDrag] = useState(null);
   //Piezas de la caja, se deben cargar desde la config
-  const [boxPieces, setBoxPieces] = useState([
-    { id: 0, name: "peon", blanca: false, class: "", position: BOXPOSITION },
-    { id: 1, name: "peon", blanca: false, class: "", position: BOXPOSITION },
-    { id: 2, name: "peon", blanca: false, class: "", position: BOXPOSITION },
-    { id: 3, name: "peon", blanca: false, class: "", position: BOXPOSITION },
-    { id: 4, name: "peon", blanca: false, class: "", position: BOXPOSITION },
-    { id: 5, name: "peon", blanca: false, class: "", position: BOXPOSITION },
-    { id: 6, name: "peon", blanca: false, class: "", position: BOXPOSITION },
-    { id: 7, name: "peon", blanca: false, class: "", position: BOXPOSITION },
-    { id: 8, name: "torre", blanca: false, class: "", position: BOXPOSITION },
-    { id: 9, name: "torre", blanca: false, class: "", position: BOXPOSITION },
-    { id: 10, name: "alfil", blanca: false, class: "", position: BOXPOSITION },
-    { id: 11, name: "alfil", blanca: false, class: "", position: BOXPOSITION },
-    { id: 12, name: "caballo", blanca: false, class: "", position: BOXPOSITION },
-    { id: 13, name: "caballo", blanca: false, class: "", position: BOXPOSITION },
-    { id: 14, name: "rey", blanca: false, class: "", position: BOXPOSITION },
-    { id: 15, name: "reina", blanca: false, class: "", position: BOXPOSITION },
-    { id: 90, name: "peon", blanca: true, class: "", position: BOXPOSITION },
-    { id: 91, name: "peon", blanca: true, class: "", position: BOXPOSITION },
-    { id: 92, name: "peon", blanca: true, class: "", position: BOXPOSITION },
-    { id: 93, name: "peon", blanca: true, class: "", position: BOXPOSITION },
-    { id: 94, name: "peon", blanca: true, class: "", position: BOXPOSITION },
-    { id: 95, name: "peon", blanca: true, class: "", position: BOXPOSITION },
-    { id: 96, name: "peon", blanca: true, class: "", position: BOXPOSITION },
-    { id: 97, name: "peon", blanca: true, class: "", position: BOXPOSITION },
-    { id: 98, name: "torre", blanca: true, class: "", position: BOXPOSITION },
-    { id: 99, name: "torre", blanca: true, class: "", position: BOXPOSITION },
-    { id: 910, name: "alfil", blanca: true, class: "", position: BOXPOSITION },
-    { id: 911, name: "alfil", blanca: true, class: "", position: BOXPOSITION },
-    { id: 912, name: "caballo", blanca: true, class: "", position: BOXPOSITION },
-    { id: 913, name: "caballo", blanca: true, class: "", position: BOXPOSITION },
-    { id: 914, name: "rey", blanca: true, class: "", position: BOXPOSITION },
-    { id: 915, name: "reina", blanca: true, class: "", position: BOXPOSITION },
-  ]);
+  const [boxPieces, setBoxPieces] = useState(ALLPICES);
 
   //Evento cuando coges una pieza
   const handleDragStart = (e, piece) => {
--- src/redux/ChessboardSlicer.jsx
+++ src/redux/ChessboardSlicer.jsx
@@ -14,7 +14,6 @@ const chessboard = createSlice({
     },
     saveSquare: (state, action) => {
       const p = action.payload;
-      //saveSquareClean(state.chessboard, p);
       state.chessboard[p.x][p.y] = p.piece;
     },
     cleanSquare: (state, action) => {
@@ -29,15 +28,10 @@ const chessboard = createSlice({
 });
 
 function saveSquareClean(chessBoard, p) {
-  try {
-    for (let x = 0; x < chessBoard.length; x++) {
-      for (let y = 0; y < chessBoard[x].length; y++) {
-        if (chessBoard[x][y] && chessBoard[x][y].id === p.piece.id) chessBoard[x][y] = null;
-      }
+  for (let x = 0; x < chessBoard.length; x++) {
+    for (let y = 0; y < chessBoard[x].length; y++) {
+      if (chessBoard[x][y] && chessBoard[x][y].id === p.piece.id) chessBoard[x][y] = null;
     }
-  } catch (error) {
-    console.log(error);
-    console.log(p);
   }
 }
 
