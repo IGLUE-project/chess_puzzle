88f487dd044eeb4f628dffa79be6501111aadd3e
Javier Ferreño <javifc67@gmail.com>
2025-03-12 19:22:02 +0100

    prueba a meter imagenes de fondo

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,29 +1,33 @@
 import { useSelector } from "react-redux";
 import { getChessboard } from "../redux/ChessboardSliceSelector";
 import Square from "./Square";
+import "../assets/scss/Board.scss";
 
 export default function Board({ handleDragEnter, handleDrop, handleDragStart, handleDragEnd, handleDragLeave }) {
   const chessboard = useSelector(getChessboard);
 
   return (
-    <div className="Board" style={{ display: "flex" }}>
-      {chessboard.map((row, x) => (
-        <div className="row" key={x}>
-          {row.map((square, y) => (
-            <Square
-              handleDrop={handleDrop}
-              handleDragEnter={handleDragEnter}
-              handleDragLeave={handleDragLeave}
-              handleDragStart={handleDragStart}
-              handleDragEnd={handleDragEnd}
-              key={x + " " + y}
-              color={(x + y) % 2 === 1 ? "black" : ""}
-              x={x}
-              y={y}
-            />
-          ))}
-        </div>
-      ))}
+    <div className="Board">
+      <img className="chessboardimg" src="/src/assets/images/chessboard.jpg" alt="chessboard" />
+      <div className="chessboard">
+        {chessboard.map((row, x) => (
+          <div className="row" key={x}>
+            {row.map((square, y) => (
+              <Square
+                handleDrop={handleDrop}
+                handleDragEnter={handleDragEnter}
+                handleDragLeave={handleDragLeave}
+                handleDragStart={handleDragStart}
+                handleDragEnd={handleDragEnd}
+                key={x + " " + y}
+                color={(x + y) % 2 === 1 ? "black" : ""}
+                x={x}
+                y={y}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
